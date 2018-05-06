b8ba845b271fd3f085c066838aa6bafea97b0d4c
Diana Bobrova <Diana_bobrova@epam.com>
2018-05-07 00:09:26 +0300

    Change structure and remove logging

--- app/components/form-list.js
+++ app/components/form-list.js
@@ -49,45 +49,26 @@ export default Component.extend({
   arrayLength: computed(function() {
     return this.get('list').length;
   }),
-  itemAt: computed( {
-    set(key, value) {
-      return this.get('list').objectAt(value);
-    }
-  }),
   actions: {
     movedItem(item, formerPosition, newPosition) {
-      this.set('itemAt', newPosition-1);
-      let itemBefore = this.get('itemAt').position;
-      this.set('itemAt', newPosition);
-      let itemAfter = this.get('itemAt').position;
+      let itemBefore = this.get('list').objectAt(newPosition-1).position;
+      let itemAfter = this.get('list').objectAt(newPosition).position;
       let diffItems = itemAfter - itemBefore;
       let average = itemBefore + Math.floor(diffItems/2);
-      console.log('itemBefore ' + itemBefore);
-      console.log('itemAfter ' + itemAfter);
-      console.log('diffItems ' + diffItems);
-      console.log(average);
       if(diffItems >= 2) {
-        console.log('again');
         set(item, 'position', average);
       } else if (this.get('arrayLength')/newPosition >= 2) {
-        /*уменьшить те, что до нуля*/
-        console.log('hi');
         set(item, 'position', itemBefore - 1);
         for(let i = newPosition-1; i >= 0; i--) {
-          this.set('itemAt', i);
-          let itemCurrent = this.get('itemAt');
-          this.set('itemAt', i+1);
-          let positionValue = this.get('itemAt').position - this.get('arrayLength');
+          let itemCurrent = this.get('list').objectAt(i);
+          let positionValue = this.get('list').objectAt(i+1).position - this.get('arrayLength');
           set(itemCurrent, 'position', positionValue);
         }
       } else {
-        /*увеличить до конца*/
-        console.log('bye');
+        set(item, 'position', itemBefore - 1);
         for(let i = newPosition+1; i < length; i++) {
-          this.set('itemAt', i);
-          let itemCurrent = this.get('itemAt');
-          this.set('itemAt', i+1);
-          let positionValue = this.get('itemAt').position + this.get('arrayLength');
+          let itemCurrent = this.get('list').objectAt(i);
+          let positionValue = this.get('list').objectAt(i+1).position + this.get('arrayLength');
           set(itemCurrent, 'position', positionValue);
         }
       }
